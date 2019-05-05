62e0f2958c7f537c384d003c4f08b59daf7e08ab
Shwetha <sa_shwetha@hotmail.com>
2019-05-05 18:05:26 +0530

    Adding navlink component

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -1,24 +1,24 @@
-import React from 'react';
-import styles from './style.css';
+import React from "react";
+import styles from "./style.css";
+import NavLink from "./NavLink";
 
 const Navbar = props => {
-    //destructuring
     const {
         activeTab,
         onTabChange
     } = props;
 
     return (
-        <nav className={styles.navbarContainer}>
-            <ul className={styles.navbarList}>
-                <li className={`${styles.navbarListItem} ${activeTab === 0 ? styles.navbarItemActive : ""}`}>
-                    <a onClick={()=>onTabChange(0)}>Items</a>
-                </li>
-                <li className={`${styles.navbarListItem} ${activeTab === 1 ? styles.navbarItemActive : ""}`}>
-                    <a onClick={()=>onTabChange(1)}>Cart</a>
-                </li>
-            </ul>
-        </nav>
+      <div className={styles.navbarContainer}>
+        <ul className={styles.navbarList}>
+          <li className={`${styles.navbarListItem} ${activeTab === 0 ? styles.navbarItemActive : ""}`}>
+            <NavLink index={0} onClick={onTabChange}>Items</NavLink>
+          </li>
+          <li className={`${styles.navbarListItem} ${activeTab === 1 ? styles.navbarItemActive : ""}`}>
+            <NavLink index={1} onClick={onTabChange}>Cart</NavLink>
+          </li>
+        </ul>
+      </div>
     );
 }
 
