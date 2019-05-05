ab081e3db82aba10402f4b324989bf57f7691a7e
Shwetha <sa_shwetha@hotmail.com>
2019-05-05 17:32:43 +0530

    Added navbar switch functionality

--- src/App.js
+++ src/App.js
@@ -1,16 +1,28 @@
-import React from 'react';
-import logo from './logo.svg';
-/*import './App.css';*/
-import style from './App.css';
+import React, { Component } from 'react';
+import styles from './App.css';
 import Navbar from './components/Navbar';
 
-function App() {
-  return (    
-      <div className={style.app}>
-      <Navbar />
+class App extends Component {
+  state = {
+    activeTab: 0
+  };
+
+  handleTabChange = index => {
+    this.setState({
+      activeTab: index
+    });
+  }
+
+  render() {
+    const { activeTab } = this.state;
+    return (
+      <div className="App">
+        <div className={styles.app}>
+          <Navbar activeTab={activeTab} onTabChange={this.handleTabChange}/>
+        </div>
       </div>
-    
-  );
+    );
+  }
 }
 
 export default App;
